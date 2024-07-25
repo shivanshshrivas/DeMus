7b89d6badbdf08d17a277dbf6dc1db06983588a5
Shivansh Shrivas <shrivasshivansh@gmail.com>
2024-07-24 21:42:32 -0500

    mino fix

--- frontend/src/services/wallet.js
+++ frontend/src/services/wallet.js
@@ -1,5 +1,5 @@
 import Web3 from 'web3';
-import Vibe from '../build/contracts/Vibe.json';
+import Vibe from '../contracts/Vibe.json';
 
 const web3 = new Web3(Web3.givenProvider || 'https://polygon-amoy.g.alchemy.com/v2/7yGZ0COisSDN-0gXm80e6RQzBJAkjkEk');
 
